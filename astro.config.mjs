f6ec7d31c608e077b5862d0b0e9656672fc7ecb9
Kyle Satti <k@kylejs.dev>
2025-06-22 17:55:25 -0500

    Use vercel static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
